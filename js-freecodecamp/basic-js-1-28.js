b136d86181daf394856ab57250944de5e96063f2
Diego Sepulveda <diegosl@hotmail.cl>
2021-05-01 17:35:02 -0400

    JS FCC 1-28

---
+++ js-freecodecamp/basic-js-1-28.js
@@ -0,0 +1,24 @@
+/* 
+    Constructing Strings with VariablesPassed
+    Sometimes you will need to build a string, Mad Libs style. 
+    By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+    Example:
+
+    var ourName = "freeCodeCamp";
+    var ourStr = "Hello, our name is " + ourName + ", how are you?";
+    ourStr would have a value of the string Hello, our name is freeCodeCamp, how are you?.
+*/
+
+/* 
+    - Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+*/
+
+/* 
+    - myName should be set to a string at least 3 characters long.
+    - You should use two + operators to build myStr with myName inside it.
+*/
+
+// Only change code below this line
+var myName = "John";
+var myStr = "My name is " + myName + " and I am well!";
