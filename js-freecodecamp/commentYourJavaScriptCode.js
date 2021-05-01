32ab29a8e5e9f500bb56eace3f09ed2dda3bfa3f
Diego Sepulveda <diegosl@hotmail.cl>
2021-05-01 14:59:41 -0400

    JS FCC 1-02

--- js-freecodecamp/commentYourJavaScriptCode.js
+++ js-freecodecamp/commentYourJavaScriptCode.js
@@ -1,25 +1,24 @@
 /* 
-Comment Your JavaScript CodePassed
-Comments are lines of code that JavaScript will intentionally ignore.
-Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+    Comment Your JavaScript CodePassed
+    Comments are lines of code that JavaScript will intentionally ignore.
+    Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
 
-There are two ways to write comments in JavaScript: 
-
-
-Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
+    There are two ways to write comments in JavaScript: 
 
 
-// This is an in-line comment.
-// You can make a multi-line comment beginning with /* and ending with */
+    Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
 
 
-// NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. 
-// Good commenting can help communicate the intent of your code—both for others and for your future self.
+    // This is an in-line comment.
+    // You can make a multi-line comment beginning with /* and ending with */
 
-// Try creating one of each type of comment:
+    // NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code.
+    // Good commenting can help communicate the intent of your code—both for others and for your future self.
 
-// You should create a // style comment that contains at least five letters.
-// You should create a /* */ style comment that contains at least five letters.
+    // Try creating one of each type of comment:
+
+    // You should create a // style comment that contains at least five letters.
+    // You should create a /* */ style comment that contains at least five letters.
 
 // Esto es un comentario de una linea
 /*  Esto es un comentario
