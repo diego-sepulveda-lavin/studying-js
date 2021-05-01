f8aecb6c42ba783d9bccf657c8f99c70b634f7ff
Diego Sepulveda <diegosl@hotmail.cl>
2021-05-01 15:27:39 -0400

    JS FCC 1-09

---
+++ js-freecodecamp/basic-js-1-09.js
@@ -0,0 +1,21 @@
+/* 
+    Subtract One Number from Another with JavaScriptPassed
+    We can also subtract one number from another.
+
+    JavaScript uses the - symbol for subtraction.
+
+    Example
+
+    myVar = 12 - 6;
+    myVar would have the value 6.
+*/
+
+/* 
+    - Change the 0 so the difference is 12.
+*/
+
+/* 
+    - The variable difference should be equal to 12.
+    - You should only subtract one number from 45.
+*/
+var difference = 45 - 33;
